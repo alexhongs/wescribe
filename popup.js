5cde9a61ea7d15f2e442b1f3c899931cf3a13cf2
Alex Hong <seongkwh@andrew.cmu.edu>
2019-05-14 11:34:18 +0900

    Basic form layout and JSON output

--- popup.js
+++ popup.js
@@ -3,6 +3,48 @@ window.onload = function(){
 
 	addButton.onclick = function(){
 		console.log("Add Button clicked!");
-		document.getElementById("subscription_name").value = "Johnny Bravo";
+
+
+		// Save input into databases
+		// sub_name = document.getElementById("name").value
+		// console.log(sub_name);
+		form = document.getElementById("form");
+		a = formToJSON(form);
+		console.log(a);
+
+		// document.getElementById("name").value = "Johnny Bravo";
 	};
 };
+
+
+function formToJSON( form ) {
+	//preventDefault()
+	var obj = {};
+	var elements = form.querySelectorAll( "input, select");
+	for( var i = 0; i < elements.length; ++i ) {
+		var element = elements[i];
+		var name = element.name;
+		var value = element.value;
+
+		if( name ) {
+			obj[ name ] = value;
+		}
+	}
+	return JSON.stringify( obj );
+}
+
+
+// Expected JSON output 
+// {
+//   ""subscription_name"": "Apple",
+//   "pay_amount": "Jason Lengstorf",
+//   "period": "I have a general question.",
+//   "pay_date": "jason@lengstorf.com",
+//   "free_period": "",
+//   "url": "1b3a9374-1a8e-434e-90ab-21aa7b9b80e7"
+// }
+
+
+
+
+
